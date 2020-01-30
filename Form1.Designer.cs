9b20db790e72d1cde10fc1ffdd2a569baeab44e2
steven0129 <h3584935@gmail.com>
2020-01-30 15:40:46 +0800

    init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Thor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBoxIn = new System.Windows.Forms.TextBox();
+            this.textBoxOut = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOut = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.displayTable = new System.Windows.Forms.TableLayoutPanel();
+            this.btnIn = new System.Windows.Forms.Button();
+            this.labelFileName = new System.Windows.Forms.Label();
+            this.labelLabel = new System.Windows.Forms.Label();
+            this.labelOriginName = new System.Windows.Forms.Label();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBoxIn
+            // 
+            this.textBoxIn.Location = new System.Drawing.Point(115, 45);
+            this.textBoxIn.Name = "textBoxIn";
+            this.textBoxIn.Size = new System.Drawing.Size(684, 29);
+            this.textBoxIn.TabIndex = 3;
+            // 
+            // textBoxOut
+            // 
+            this.textBoxOut.Location = new System.Drawing.Point(115, 80);
+            this.textBoxOut.Name = "textBoxOut";
+            this.textBoxOut.Size = new System.Drawing.Size(684, 29);
+            this.textBoxOut.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "輸入Info";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "輸出Info";
+            // 
+            // btnOut
+            // 
+            this.btnOut.Location = new System.Drawing.Point(805, 80);
+            this.btnOut.Name = "btnOut";
+            this.btnOut.Size = new System.Drawing.Size(75, 29);
+            this.btnOut.TabIndex = 7;
+            this.btnOut.Text = "...";
+            this.btnOut.UseVisualStyleBackColor = true;
+            this.btnOut.Click += new System.EventHandler(this.btnOut_Click);
+            // 
+            // displayTable
+            // 
+            this.displayTable.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.displayTable.AutoSize = true;
+            this.displayTable.ColumnCount = 16;
+            this.displayTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.Location = new System.Drawing.Point(32, 115);
+            this.displayTable.Margin = new System.Windows.Forms.Padding(1);
+            this.displayTable.Name = "displayTable";
+            this.displayTable.RowCount = 16;
+            this.displayTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.displayTable.Size = new System.Drawing.Size(848, 607);
+            this.displayTable.TabIndex = 8;
+            // 
+            // btnIn
+            // 
+            this.btnIn.Location = new System.Drawing.Point(805, 45);
+            this.btnIn.Name = "btnIn";
+            this.btnIn.Size = new System.Drawing.Size(75, 29);
+            this.btnIn.TabIndex = 9;
+            this.btnIn.Text = "...";
+            this.btnIn.UseVisualStyleBackColor = true;
+            this.btnIn.Click += new System.EventHandler(this.btnIn_Click);
+            // 
+            // labelFileName
+            // 
+            this.labelFileName.AutoSize = true;
+            this.labelFileName.Location = new System.Drawing.Point(1007, 9);
+            this.labelFileName.Name = "labelFileName";
+            this.labelFileName.Size = new System.Drawing.Size(81, 18);
+            this.labelFileName.TabIndex = 10;
+            this.labelFileName.Text = "File Name";
+            // 
+            // labelLabel
+            // 
+            this.labelLabel.AutoSize = true;
+            this.labelLabel.Location = new System.Drawing.Point(1007, 32);
+            this.labelLabel.Name = "labelLabel";
+            this.labelLabel.Size = new System.Drawing.Size(47, 18);
+            this.labelLabel.TabIndex = 11;
+            this.labelLabel.Text = "Label";
+            // 
+            // labelOriginName
+            // 
+            this.labelOriginName.AutoSize = true;
+            this.labelOriginName.Location = new System.Drawing.Point(1007, 56);
+            this.labelOriginName.Name = "labelOriginName";
+            this.labelOriginName.Size = new System.Drawing.Size(111, 18);
+            this.labelOriginName.TabIndex = 12;
+            this.labelOriginName.Text = "Original Name";
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(1010, 83);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 40);
+            this.btnPrev.TabIndex = 13;
+            this.btnPrev.Text = "<--";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(1091, 83);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 40);
+            this.btnNext.TabIndex = 14;
+            this.btnNext.Text = "-->";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(886, 83);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(92, 26);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.Text = "儲存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1299, 746);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.labelOriginName);
+            this.Controls.Add(this.labelLabel);
+            this.Controls.Add(this.labelFileName);
+            this.Controls.Add(this.btnIn);
+            this.Controls.Add(this.displayTable);
+            this.Controls.Add(this.btnOut);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxOut);
+            this.Controls.Add(this.textBoxIn);
+            this.Name = "Form1";
+            this.Text = "Thor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBoxIn;
+        private System.Windows.Forms.TextBox textBoxOut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOut;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TableLayoutPanel displayTable;
+        private System.Windows.Forms.Button btnIn;
+        private System.Windows.Forms.Label labelFileName;
+        private System.Windows.Forms.Label labelLabel;
+        private System.Windows.Forms.Label labelOriginName;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thor
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[] picboxArr = new PictureBox[256];
+        int counter = 0;
+        string[] info_csv;
+        string out_info = "";
+        bool[] noiseArr = new bool[256];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 載入圖片ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "載入圖片";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                textBoxIn.Text = fileName;
+                string[] fileNameArr = fileName.Split(new char[] { '\\' });
+                string currName = fileNameArr[fileNameArr.Length - 1];
+
+                Bitmap img = new Bitmap(fileName);
+                int blockWidth = 240;
+                int blockHeight = 135;
+
+                for (int idxY = 0; idxY < 16; idxY++)
+                {
+                    for (int idxX = 0; idxX < 16; idxX++)
+                    {
+                        Bitmap block = new Bitmap(blockWidth, blockHeight);
+                        Graphics graphic = Graphics.FromImage(block);
+                        graphic.DrawImage(img, new Rectangle(0, 0, blockWidth, blockHeight), new Rectangle(blockWidth * idxX, blockHeight * idxY, blockWidth, blockHeight), GraphicsUnit.Pixel);
+
+                        picboxArr[idxY * 16 + idxX].Image = block;
+                    }
+                }
+            }
+        }
+
+        private void btnOut_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.Title = "匯出Info檔";
+            openFileDialog2.Filter = "csv file (*.csv)|*.csv";
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                out_info = openFileDialog2.FileName;
+                textBoxOut.Text = out_info;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < picboxArr.Length; i++)
+            {
+                picboxArr[i] = new PictureBox();
+                picboxArr[i].Size = new Size(30, 15);
+                picboxArr[i].Anchor = (AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom);
+                picboxArr[i].SizeMode = PictureBoxSizeMode.Zoom;
+                picboxArr[i].Click += new EventHandler(this.picbox_handler);
+                displayTable.Controls.Add(picboxArr[i]);
+            }
+        }
+
+        private void picbox_handler(object sender, EventArgs e)
+        {
+            PictureBox picbox = (PictureBox)sender;
+            Graphics g = picbox.CreateGraphics();
+            g.DrawRectangle(new Pen(Color.Red, 4), 0, 0, picbox.Width, picbox.Height);
+            noiseArr[Array.IndexOf(picboxArr, sender)] = true;
+        }
+
+        private void btnIn_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "載入Info檔";
+            openFileDialog1.Filter = "csv file (*.csv)|*.csv";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                counter = int.Parse(File.ReadAllText(Path.GetDirectoryName(fileName) + "/temp.txt"));
+                textBoxIn.Text = fileName;
+
+                info_csv = File.ReadAllLines(fileName).Skip(1).ToArray();
+                string[] info = info_csv[counter].Split(new char[] { ',' });
+                this.load_img(Path.GetDirectoryName(fileName) + "/" + info[0]);
+
+                labelFileName.Text = info[0];
+                labelLabel.Text = info[1];
+                labelOriginName.Text = info[2];
+            }
+        }
+
+        private void load_img(string fileName)
+        {
+            fileName = fileName.Replace('/', '\\');
+            string[] fileNameSplit = fileName.Split('\\');
+            Bitmap img = new Bitmap(fileName);
+            int blockWidth = 240;
+            int blockHeight = 135;
+
+            for (int idxY = 0; idxY < 16; idxY++)
+            {
+                for (int idxX = 0; idxX < 16; idxX++)
+                {
+                    Bitmap block = new Bitmap(blockWidth, blockHeight);
+                    Graphics graphic = Graphics.FromImage(block);
+                    graphic.DrawImage(img, new Rectangle(0, 0, blockWidth, blockHeight), new Rectangle(blockWidth * idxX, blockHeight * idxY, blockWidth, blockHeight), GraphicsUnit.Pixel);
+                    picboxArr[idxY * 16 + idxX].Image = block;
+                }
+            }
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            btnSave.Enabled = true;
+            string fileName = openFileDialog1.FileName;
+            if (counter != 0)
+            {
+                counter--;
+                string[] info = info_csv[counter].Split(new char[] { ',' });
+                this.load_img(Path.GetDirectoryName(fileName) + "/" + info[0]);
+
+                labelFileName.Text = info[0];
+                labelLabel.Text = info[1];
+                labelOriginName.Text = info[2];
+
+                for (int i = 0; i < noiseArr.Length; i++) noiseArr[i] = false;
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            btnSave.Enabled = true;
+            string fileName = openFileDialog1.FileName;
+            if (counter != info_csv.Length)
+            {
+                counter++;
+                string[] info = info_csv[counter].Split(new char[] { ',' });
+                this.load_img(Path.GetDirectoryName(fileName) + "/" + info[0]);
+
+                labelFileName.Text = info[0];
+                labelLabel.Text = info[1];
+                labelOriginName.Text = info[2];
+
+                for (int i = 0; i < noiseArr.Length; i++) noiseArr[i] = false;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string[] info = info_csv[counter].Split(new char[] { ',' });
+            string out_dir = Path.GetDirectoryName(out_info);
+            for (int i = 0; i < picboxArr.Length; i++)
+            {
+                PictureBox picbox = picboxArr[i];
+
+                string out_name = $"{info[0].Substring(0, info[0].Length - 4)}_{i}.bmp";
+                picbox.Image.Save($"{out_dir}/{out_name}", ImageFormat.Bmp);
+                if (noiseArr[i] == true)
+                    File.AppendAllText(openFileDialog2.FileName, $"\n{out_name},Noise,{info[0]},{info[2]}");
+                else
+                    File.AppendAllText(openFileDialog2.FileName, $"\n{out_name},{info[1]},{info[0]},{info[2]}");
+            }
+
+            btnSave.Enabled = false;
+            File.WriteAllText(Path.GetDirectoryName(openFileDialog1.FileName) + "/temp.txt", counter.ToString());
+        }
+    }
+}
